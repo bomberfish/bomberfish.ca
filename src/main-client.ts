0e3965b97881de93d000ce06659415708a61d38b
bomberfish <hariz@bomberfish.ca>
2025-10-14 22:27:15 -0400

    css variable polyfill for legacy browsers

--- src/main-client.ts
+++ src/main-client.ts
@@ -2,8 +2,13 @@ console.log("hiiii :3");
 import { hydrate } from "dreamland/ssr/client";
 import { oneko } from "./Oneko";
 import App from "./App";
+import cssVars from "css-vars-ponyfill";
 
 oneko();
+
+cssVars({
+	watch: true
+});
 
 hydrate(
 	App,
