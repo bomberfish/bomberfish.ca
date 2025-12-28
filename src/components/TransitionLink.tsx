5ec782fda1863a07abe407f29ede577fb937af77
bomberfish <hariz@bomberfish.ca>
2025-12-28 03:06:30 -0500

    work around weird gh routing stuff

--- src/components/TransitionLink.tsx
+++ src/components/TransitionLink.tsx
@@ -41,7 +41,7 @@ const normalizePath = (path: string) => {
 	return normalized.length === 0 ? "/" : normalized;
 };
 
-const NAV_PATHS = ["/", "/projects", "/blog", "/siteinfo"];
+const NAV_PATHS = ["/", "/projects/index", "/blog/index", "/siteinfo"];
 
 const getNavigationOrder = (path: string): number | undefined => {
 	const normalized = normalizePath(path);
