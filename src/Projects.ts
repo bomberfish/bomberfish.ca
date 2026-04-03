87456df5e88a80d91d5e5b0f9e1723ba531197e1
bomberfish <hariz@bomberfish.ca>
2026-04-03 17:48:58 -0400

    move projectcarddetails definition to projects.ts

--- src/Projects.ts
+++ src/Projects.ts
@@ -1,4 +1,44 @@
-import ProjectCardDetails from "./types/Project";
+class ProjectCardDetails {
+	img: string | undefined;
+	title: string;
+	blurb: string;
+	startYear: number;
+	endYear?: number;
+	largeDesc: string;
+	links?: { name: string; url: string; icon?: string }[];
+	featured?: boolean;
+	featuredPosition?: number | undefined;
+	lastPathComponent: string;
+
+	constructor(
+		imgURL: string | undefined,
+		title: string,
+		blurb: string,
+		largeDesc: string | undefined,
+		startYear: number,
+		endYear?: number,
+		links?: { name: string; url: string; icon?: string }[],
+		featured?: boolean,
+		featuredPosition?: number | undefined,
+		lastPathComponent?: string
+	) {
+		this.img = imgURL;
+		this.title = title;
+		this.blurb = blurb;
+		this.startYear = startYear;
+		this.endYear = endYear;
+		this.largeDesc = largeDesc || blurb;
+		this.links = links;
+		this.featured = featured;
+		this.featuredPosition = featuredPosition;
+		this.lastPathComponent =
+			lastPathComponent ||
+			title
+				.toLowerCase()
+				.replace(/\s+/g, "-")
+				.replace(/[^\w-]/g, "");
+	}
+}
 
 export const jobs = [
 	new ProjectCardDetails(
